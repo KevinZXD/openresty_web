39cc4e543516535a162159e0a7eff9f771930825
xudong12 <xudong12@staff.weibo.com>
2020-04-23 13:54:13 +0800

    commit

--- web/gateway.lua
+++ web/gateway.lua
@@ -3,7 +3,8 @@
 --- Created by xudong12.
 --- DateTime: 2020/2/26 1:25 PM
 ---
-local core_t = require('trends.core')
-local core = core_t:new()
-core:run()
---ngx.say("ok")
+--local core_t = require('trends.core')
+--local core = core_t:new()
+--core:run()
+ENV_DATACENTER = os.getenv('ENV_DATACENTER')
+ngx.say(ENV_DATACENTER)
